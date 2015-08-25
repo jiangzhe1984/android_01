13b9d3a84501697f9b77e4b6c2e401a91edc2138
jiangzhe <jiangzhe1984@163.com>
2015-08-25 17:28:31 +0800

    20150825

--- app/src/main/java/com/example/jiangz/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/jiangz/myapplication/RegisterActivity.java
@@ -38,6 +38,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         switch (v.getId()){
             case R.id.save :
 
+                
                 Userinfo userinfo = new Userinfo();
                 userinfo.setUsername(username.getText().toString());
                 userinfo.setPassword(password.getText().toString());
